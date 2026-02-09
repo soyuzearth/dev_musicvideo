798e8e4be32cbf9c3964cbff7321cbebffc3b577
MJKIM <soyuz.earth@gmail.com>
2026-02-09 20:48:10 +0900

    docs: add thumbnail update script for jinwoo book

---
+++ seed_add_thumbnail_jinwoo.sql
@@ -0,0 +1,6 @@
+-- Update thumbnail for '용감한 진우와 불꽃 괴물'
+UPDATE projects 
+SET 
+    thumbnail_url = 'https://udlyikebeopjnewoydbg.supabase.co/storage/v1/object/public/ebooks/Thumbnail_BraveJinwoo.png'
+WHERE 
+    title = '용감한 진우와 불꽃 괴물' AND author = '김미진';
